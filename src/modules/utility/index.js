f868b14df72c01ff1b99777a915fdf2e3c786126
goldfarbas <41171114+goldfarbas@users.noreply.github.com>
2021-07-16 07:57:02 -0700

    Revert "Revert "Merge pull request #18 from Content-Production/qrcode""
    This reverts commit 3922345567e7678199d718dce035fa7d9bf90464.

--- src/modules/utility/index.js
+++ src/modules/utility/index.js
@@ -2,3 +2,4 @@ export { default as Video } from './Video';
 export { default as Aligner } from './Aligner';
 export { default as SEO } from './seo.js';
 export { default as Indent } from './Indent';
+export { default as QRCodeGen } from './QRCodeGen.js';
